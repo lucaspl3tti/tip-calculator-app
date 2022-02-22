b30fa241486b610916f34b9c6f7bd8f42a5b1cb4
Jan-Luca Splettstößer <jan-luca@spletti-din.de>
2022-02-22 01:18:10 +0100

    Delete fonts and fix js coding style

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
 import '../scss/app.scss';
 import CalculateTipPlugin from './tip-calculator.js';
 
-let CalculateTipPlugin = new CalculateTipPlugin('#tipCalculatorForm');
+let calculateTipPlugin = new CalculateTipPlugin('#tipCalculatorForm');
